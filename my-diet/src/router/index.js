029b01668d6a83c902245e440b4d41a6fd966f75
ezar31 <akungugelezar@gmail.com>
2022-12-06 22:05:39 +0700

    setup vue option

--- my-diet/src/router/index.js
+++ my-diet/src/router/index.js
@@ -1,23 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+// import LoginRegister from "../views/LoginRegister.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: HomeView,
+      name: 'home'
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/login',
+    //   component: LoginRegister,
+    //   name: 'login'
+    // },
   ]
 })
+
+// router.beforeEach(async (to, from, next) => {
+//   if (!localStorage.access_token && to.name == 'FavPage') {
+//     next({ name: 'login' })
+//   } else if (localStorage.access_token && to.name == 'login') {
+//     next({ name: 'home' })
+//   } else {
+//     next()
+//   }
+// })
 
 export default router
