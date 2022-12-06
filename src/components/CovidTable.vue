845ba28403f5c1724496001b97b6871646b7a129
alammooo <abdullah.alamm@gmail.com>
2022-12-07 01:52:09 +0700

    add covid data from API

--- src/components/CovidTable.vue
+++ src/components/CovidTable.vue
@@ -1,8 +1,12 @@
 <script>
 import { mapActions, mapWritableState } from "pinia"
 import { useCounterStore } from "../stores/counter"
+import TableRow from "../components/TableRow.vue"
 
 export default {
+  components:{
+    TableRow,
+  },
   methods: {
     ...mapActions(useCounterStore, ["fetchCovidData"]),
   },
@@ -21,27 +25,16 @@ export default {
         <table class="w-full text-left text-gray-500">
           <thead class="text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th scope="col" class="py-3 px-6">Product name</th>
-              <th scope="col" class="py-3 px-6">Color</th>
-              <th scope="col" class="py-3 px-6">Category</th>
-              <th scope="col" class="py-3 px-6">Price</th>
+              <th scope="col" class="py-3 px-6">State</th>
+              <th scope="col" class="py-3 px-6">Total Case</th>
+              <th scope="col" class="py-3 px-6">Total Cured</th>
+              <th scope="col" class="py-3 px-6">Total Dead</th>
+              <th scope="col" class="py-3 px-6">Total Patient</th>
               <th scope="col" class="py-3 px-6">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b" v-for="list in covidData" :key="key">
-              <th
-                scope="row"
-                class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-                {{ list.key }}
-              </th>
-              <td class="py-4 px-6">Sliver</td>
-              <td class="py-4 px-6">Laptop</td>
-              <td class="py-4 px-6">$2999</td>
-              <td class="py-4 px-6">
-                <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
-              </td>
-            </tr>
+            <TableRow v-for="list in covidData" :key="key" :list="list" />
           </tbody>
         </table>
       </div>
---
+++ src/components/TableRow.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      showTable: false,
+    }
+  },
+  props: ["list"],
+}
+</script>
+<template>
+  <tr class="bg-white border-b">
+    <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+      {{ list.key }}
+    </th>
+    <td class="py-4 px-6">{{ list.jumlah_kasus }}</td>
+    <td class="py-4 px-6">{{ list.jumlah_sembuh }}</td>
+    <td class="py-4 px-6">{{ list.jumlah_meninggal }}</td>
+    <td class="py-4 px-6">{{ list.jumlah_dirawat }}</td>
+    <td class="py-4 px-6">
+      <a
+        @click.prevent="showTable = !showTable"
+        class="font-medium text-blue-600 hover:underline cursor-pointer select-none"  
+        >Show Detail</a
+      >
+    </td>
+  </tr>
+  <div class="absolute bg-white p-5 w-1/2 shadow-xl border" v-if="showTable">
+    <table class="w-full text-left text-gray-500 my-5">
+      <thead class="text-gray-700 uppercase bg-sky-100">
+        <tr>
+          <th scope="col" class="py-3 px-6">Male</th>
+          <th scope="col" class="py-3 px-6">Female</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white border-b">
+          <td class="py-4 px-6" v-for="listKelamin in list.jenis_kelamin">
+            {{ listKelamin.doc_count }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table class="w-full text-left text-gray-500">
+      <thead class="text-gray-700 uppercase bg-sky-100">
+        <tr>
+          <th scope="col" class="py-3 px-6">0-5</th>
+          <th scope="col" class="py-3 px-6">6-18</th>
+          <th scope="col" class="py-3 px-6">19-30</th>
+          <th scope="col" class="py-3 px-6">31-45</th>
+          <th scope="col" class="py-3 px-6">46-59</th>
+          <th scope="col" class="py-3 px-6">>59</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white border-b">
+          <td class="py-4 px-6" v-for="listUmur in list.kelompok_umur">
+            {{ listUmur.doc_count }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
