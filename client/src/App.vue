a64d097425c6dc4f7cc29d5269747b8290e0532b
Eris Susanto <erissusanto997@gmail.com>
2022-12-08 05:25:38 +0700

    login-regs-and-home-fiture done

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { globalStore } from './stores/global';
+import { mapActions, mapWritableState } from 'pinia';
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  computed: {
+    ...mapWritableState(globalStore, ['isLogin', 'greetTheName'])
+  },
+
+  created(){
+    if(localStorage.access_token){
+      this.greetTheName = localStorage.name
+      this.isLogin = true
+    } else{
+      this.isLogin = false
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,115 @@
+<script>
+import Card from "@/components/CardPlayer.vue";
+import Navbar from "@/components/Navbar.vue";
+import { mapActions, mapWritableState } from "pinia";
+import { globalStore } from "../stores/global";
+
+export default {
+    components: {
+        Card,
+        Navbar
+    },
+
+    computed: {
+      ...mapWritableState(globalStore, ['players', 'currentPage'])
+    },
+
+    methods: {
+      ...mapActions(globalStore, ['fetchPlayer', 'addMyPlayer']),
+
+      onClickCurrent(page){
+      this.currentPage = page
+      this.fetchPlayer()
+    },
+
+    onClickPrev(){
+      if(this.currentPage > 1){
+        this.currentPage = this.currentPage - 1
+      }
+      this.fetchPlayer()
+    },
+
+    onClickNext(){
+      this.currentPage = this.currentPage + 1
+      this.fetchPlayer()
+    },
+
+    style(page){
+      if(this.currentPage === page){
+        return "active"
+      }
+    },
+
+    stylePrevNext(page){
+      if(this.currentPage === page){
+        return "disabled"
+      }
+    },
+
+    },
+
+    created(){
+      this.fetchPlayer()
+    }
+}
+</script>
+
+<template>
+    <Navbar />
+    <section id="home-section">
+      <div class="row">
+
+        <!-- Landing Page -->
+        <div class="container-xxl" style="background-color: gray;">
+          <div class="container my-5 py-5">
+            <div class="row align-items-center g-5">
+              <div class="col-lg-6 text-center text-lg-start">
+                <h1 class="display-3 text-white animated slideInLeft">Transfer<br>Market</h1>
+                <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+              </div>
+              <div class="col-lg-6 text-center text-lg-end overflow-hidden">
+                <img class="img-fluid" src="https://cdn.pixabay.com/photo/2018/09/30/22/12/silhouette-3714836_1280.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Landing Page -->
+
+        <!-- Menu -->
+        <section class="col-md-12 text-white mt-3">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 ms-3 mb-3 border-bottom border-warning">
+            <h1 class="fw-bold text-secondary">Players</h1>
+          </div>
+          <div class="container-fluid">
+            <div class="row g-4">
+              <Card v-for="player in players.players" :key="player.id" :player="player"/>
+            </div>
+          </div>
+
+          <nav class="ms-4 mt-3" aria-label="...">
+              <ul class="pagination">
+                <li class="page-item" :class="stylePrevNext(1)">
+                  <a @click.prevent="onClickPrev" class="page-link">Previous</a>
+                </li>
+                <li class="page-item" :class="style(1)">
+                  <a @click.prevent="onClickCurrent(1)" class="page-link">1</a>
+                </li>
+                <li class="page-item" :class="style(2)">
+                  <a @click.prevent="onClickCurrent(2)" class="page-link" href="#">2</a>
+                </li> 
+                <li class="page-item" :class="style(3)">
+                  <a @click.prevent="onClickCurrent(3)" class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item" :class="style(4)">
+                  <a @click.prevent="onClickCurrent(4)" class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item" :class="stylePrevNext(4)">
+                  <a @click.prevent="onClickNext" class="page-link" href="#">Next</a>
+                </li>
+              </ul>
+          </nav>
+        </section>
+        <!-- End Menu -->
+      </div>
+    </section>
+</template>
