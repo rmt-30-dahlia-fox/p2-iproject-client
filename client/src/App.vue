bb086ba1881b1ed86c4e1327fcaefd0d03ddaed6
Neko-Life <nekolife123579@gmail.com>
2022-12-11 16:08:00 +0700

    fix indentation

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@ import {mapActions, mapState} from "pinia";
 import Navbar from "./components/Navbar.vue";
 import FriendCard from "./components/FriendCard.vue";
 import {useGlobalStore} from "./stores/global";
-    
+
 // window.fbAsyncInit = function() {
 //   FB.init({
 //     appId            : '434716772000667',
@@ -41,32 +41,40 @@ export default {
 <template>
   <Navbar v-if="!['/login', '/signup',].includes($route.path)" />
   <!-- 
-  <form action="http://localhost:3000/profile" method="post" enctype="multipart/form-data">
+    <form action="http://localhost:3000/profile" method="post" enctype="multipart/form-data">
     <input type="file" name="avatar" />
     <input type="submit">
-  </form>
-  <img src="http://localhost:3000/media/dea808a87770d2478cf6d1f0520ba110">
+    </form>
+    <img src="http://localhost:3000/media/dea808a87770d2478cf6d1f0520ba110">
   -->
-<section :class="{flex: true, 'justify-center': !user || $route.path === '/profile', 'items-center': !user || $route.path === '/profile'}">
-  <router-view></router-view>
-  <div v-if="user && !['/profile'].includes($route.path)" id="friend-list" class="w-1/5 p-4 shadow-lg h-100 bg-gray-50">
-    <h1>People</h1>
-    <div @click.prevent="goGlobal()" v-if="!['/global',].includes($route.path)" class="flex p-2 pr-6 my-2 rounded-lg cursor-pointer hover:bg-gray-200">
-      <img
-	  alt="Profile"
-	  src="https://www.texturesbackgrounds.com/files/large/world-globe-icon.jpg"
-	  class="object-cover w-10 h-10 rounded-full"
-	/>
-	<div class="flex items-center">
-	  <p class="hidden ml-2 text-xs text-left sm:block">
+  <section :class="{flex: true, 'justify-center': !user || $route.path === '/profile', 'items-center': !user || $route.path === '/profile'}">
+    <router-view></router-view>
+    <div v-if="user && !['/profile'].includes($route.path)" id="friend-list" class="w-1/5 p-4 shadow-lg h-100 bg-gray-50">
+      <h1>People</h1>
+      <div
+	  @click.prevent="goGlobal()"
+	  v-if="!['/global',].includes($route.path)"
+	  class="flex p-2 pr-6 my-2 rounded-lg cursor-pointer hover:bg-gray-200"
+	  >
+	  <img
+	      alt="Profile"
+	      src="https://www.texturesbackgrounds.com/files/large/world-globe-icon.jpg"
+	      class="object-cover w-10 h-10 rounded-full"
+	      />
+	  <div class="flex items-center">
+	    <p class="hidden ml-2 text-xs text-left sm:block">
 	    <strong class="font-medium">Global</strong><br/>
 	    <span class="text-gray-500">Join global chat</span>
-	  </p>
-	</div>
+	    </p>
+	  </div>
+      </div>
+      <FriendCard
+	  v-for="(data, i) in users.filter(u => u.id !== Number($route.params.id))"
+	  :key="data.id"
+	  :data="data"
+	  />
     </div>
-    <FriendCard v-for="(data, i) in users.filter(u => u.id !== Number($route.params.id))" :key="data.id" :data="data" />
-  </div>
-</section>
+  </section>
 </template>
 
 <style>
